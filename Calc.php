757fba68347188c057ff5f2ce6d5e58c7540538e
Roman Vesely <crazko@gmail.com>
2017-01-17 08:42:38 +0100

    added second test

---
+++ Calc.php
@@ -0,0 +1,13 @@
+<?php
+
+class Calc
+{
+	/**
+	 * @param int $a
+	 * @param int $b
+	 */
+	public function sum($a, $b)
+	{
+		# code...
+	}
+}
---
+++ CalcTest.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'Calc.php';
+
+class CalcTest extends PHPUnit_Framework_TestCase
+{
+	public function testSum()
+	{
+		$calc = new Calc;
+		$result = $calc->sum(1, 2);
+
+		$this->assertEquals(3, $result);
+	}
+}
